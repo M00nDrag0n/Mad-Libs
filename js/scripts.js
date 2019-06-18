f26b3ec18e48ff583ab741bb60d69e4e55c20e0a
Student <example@example.com>
2019-06-17 20:46:20 -0700

    save arrays and stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
-  $("#blanks form").submit(function() {
-    $(".person1").append("blah blah");
-    $(".person2").append("blah blah");
-    $(".animal").append("blah blah");
-    $(".exclamation").append("blah blah");
-    $(".verb").append("blah blah");
-    $(".noun").append("blah blah");
+  $("#blanks form").submit(function(event) {
+    var blanks = ["person1", "person2", "animal", "noun", "exclamation", "verb" "bodypart", "food", "liquid", "place", "adjective", "object", "furniture", "vehicle", "clothing", "material", "celestialbody", "exacttime", "timeofday", "adverb", "weathercondition"];
+
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+
+    });
 
     $("#story").show();
 
