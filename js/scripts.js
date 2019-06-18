c684f20fc0077e5c5da324ab996091b429b8ae14
Student <example@example.com>
2019-06-17 20:55:39 -0700

    save the comma

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var blanks = ["person1", "person2", "animal", "noun", "exclamation", "verb" "bodypart", "food", "liquid", "place", "adjective", "object", "furniture", "vehicle", "clothing", "material", "celestialbody", "exacttime", "timeofday", "adverb", "weathercondition"];
+    var blanks = ["person1", "person2", "animal", "noun", "exclamation", "verb", "bodypart", "food", "liquid", "place", "adjective", "object", "furniture", "vehicle", "clothing", "material", "celestialbody","exacttime","timeofday", "adverb", "weathercondition"];
 
     blanks.forEach(function(blank) {
       var userInput = $("input#" + blank).val();
